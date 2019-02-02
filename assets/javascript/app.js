cd2b5ed22b4aba5d9b63537afb6843636e9be65e
w-j-gannon <wgannon@austin.utexas.edu>
2019-02-02 16:00:15 -0600

    search fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,7 +32,49 @@ $(document).ready(function() {
         addButton();
     });
     
-    
+    function addGIFs(x) {
+        for (i = 0; i < 10; i ++) {
+            var divOfGIF = $("<div>");
+            var newGIF = $("<img>");
+            var ratingGIF = $("<p>");
+            var titleGIF = $("<p>");
+            var sourceURL = x.data[i].images.fixed_height_still.url;
+            divOfGIF.attr("class", "image-box");
+            newGIF.attr("src", sourceURL);
+            newGIF.attr("image-still", sourceURL);
+            newGIF.attr("image-animate", x.data[i].images.fixed_height.url);
+            newGIF.attr("state", "still");
+            newGIF.attr("class", "gif-image")
+            $("#gif-card").prepend(divOfGIF);
+            $(divOfGIF).append(newGIF);
+            ratingGIF.attr("class", "rating-text");
+            ratingGIF.text("Rating:" + x.data[i].rating);
+            titleGIF.attr("class", "title-text");
+            titleGIF.text("Title:" + x.data[i].title);
+            $(divOfGIF).append(ratingGIF);
+            $(divOfGIF).append(titleGIF);
+        } 
+    }
+
+    // AJAX to the API
+    $(document).on("click", ".search-button", function() {
+        $("#gif-card").show();
+        var titleTag = $(this).attr("data-source");
+        var APItag = titleTag;
+        var APIURL = "https://api.giphy.com/v1/gifs/search?q=" + APItag + "&api_key=" + APIkey + "&limit=5";
+        console.log(this);
+
+        $.ajax({
+            url: APIURL,
+            method: "GET"
+            }).done(function(response) {
+                addGIFs(response);
+                var GIFsTitle = $("<h2>");
+                GIFsTitle.text(titleTag);
+                $("#gif-card").prepend(GIFsTitle);
+            });
+        console.log(response);
+    });
     //};
 
 
