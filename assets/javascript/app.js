7f5300493cab964f1ed7139ff4fe810fc19665b6
w-j-gannon <wgannon@austin.utexas.edu>
2019-02-02 17:21:08 -0600

    still missing gif titles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function() {
     console.log("hello");
-    $("#gif-card").hide();
+    $("#gif-container").hide();
 
     // window.onload = function() {
 
@@ -32,33 +32,40 @@ $(document).ready(function() {
         addButton();
     });
     
+    // Take the reponse data and populate the gif cards
     function addGIFs(x) {
         for (i = 0; i < 10; i ++) {
-            var divOfGIF = $("<div>");
-            var newGIF = $("<img>");
-            var ratingGIF = $("<p>");
-            var titleGIF = $("<p>");
+            // variables
+            var newGifDiv = $("<div class='card' id='each-gif'>");
+            var newGIF = $("<img class='card-img-top'>");
+            var newGifBody = $("<div class='card-body'>");
+            var ratingGIF = $("<p class='card-text'>");
+            var titleGIF = $("<h5 class='card-title>");
             var sourceURL = x.data[i].images.fixed_height_still.url;
-            divOfGIF.attr("class", "image-box");
+            
+            //give attributes
+            
             newGIF.attr("src", sourceURL);
             newGIF.attr("image-still", sourceURL);
             newGIF.attr("image-animate", x.data[i].images.fixed_height.url);
             newGIF.attr("state", "still");
-            newGIF.attr("class", "gif-image")
-            $("#gif-card").prepend(divOfGIF);
-            $(divOfGIF).append(newGIF);
-            ratingGIF.attr("class", "rating-text");
-            ratingGIF.text("Rating:" + x.data[i].rating);
-            titleGIF.attr("class", "title-text");
-            titleGIF.text("Title:" + x.data[i].title);
-            $(divOfGIF).append(ratingGIF);
-            $(divOfGIF).append(titleGIF);
+            
+            // append gif card to div and add texts
+            $(newGifDiv).append(newGifBody)
+            $("#gif-card").prepend(newGifDiv);
+            $(newGifDiv).append(newGIF);
+            //ratingGIF.attr("class", "rating-text");
+            ratingGIF.text("Rating: " + x.data[i].rating);
+            //titleGIF.attr("class", "title-text");
+            titleGIF.text("Title: " + x.data[i].title);
+            $(newGifDiv).append(ratingGIF);
+            $(newGifBody).append(titleGIF);
         } 
-    }
+    };
 
     // AJAX to the API
     $(document).on("click", ".search-button", function() {
-        $("#gif-card").show();
+        $("#gif-container").show();
         var titleTag = $(this).attr("data-source");
         var APItag = titleTag;
         var APIURL = "https://api.giphy.com/v1/gifs/search?q=" + APItag + "&api_key=" + APIkey + "&limit=5";
@@ -71,12 +78,30 @@ $(document).ready(function() {
                 addGIFs(response);
                 var GIFsTitle = $("<h2>");
                 GIFsTitle.text(titleTag);
-                $("#gif-card").prepend(GIFsTitle);
+                $("#gif-container").prepend(GIFsTitle);
             });
         console.log(response);
     });
     //};
 
+    // Animate gifs on click
+    $(document).on("click", "img", function() {
+        
+        var GIFstate = $(this).attr("state");
+        console.log(this);
+        console.log(GIFstate);
+
+        if (GIFstate === "still") {
+
+            $(this).attr("src", $(this).attr("image-animate"));
+            $(this).attr("state", "playing");
+
+        } else if (GIFstate === "playing") {
+
+            $(this).attr("src", $(this).attr("image-animate"));
+            $(this).attr("state", "still");
+        }
+    }); 
 
 
 
