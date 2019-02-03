2c9d8cddb8f0f4fcdb57f8e544975e53dbbf95e0
w-j-gannon <wgannon@austin.utexas.edu>
2019-02-02 18:06:03 -0600

    still messing with it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,20 +6,19 @@ $(document).ready(function() {
     console.log("hello");
     $("#gif-container").hide();
 
-    // window.onload = function() {
+    // window.onload = function() {};
 
     // API key from GIPHY
     var APIkey = "BFByOJzeEKQ5fOyhPUyS6VPFT1cjcMx3";
     
     // Grab the search input and create a button
     function addButton() {
-        var newButton = $("<a>");
+        var newButton = $("<a href='#' class='btn btn-success search-button' role='button'>");
         var searchInput = $("#input-field").val().trim();
         if (searchInput.length > 0) {
             console.log(searchInput);
             $("#button-row").append(newButton);
             newButton.attr("data-source", searchInput);
-            newButton.attr("class", "search-button btn btn-success");
             newButton.text(searchInput);
         } else {
         }
@@ -82,7 +81,6 @@ $(document).ready(function() {
             });
         console.log(response);
     });
-    //};
 
     // Animate gifs on click
     $(document).on("click", "img", function() {
