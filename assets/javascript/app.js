8f65e4770f8a32c753948df0482bbba0ce938657
w-j-gannon <wgannon@austin.utexas.edu>
2019-02-02 21:17:15 -0600

    mostly working, still double and pause bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,16 @@
 // ajax with GIPHY API
-// Get gifs and insert into card bodies
-// ratings, play/pause
+
+// Enter a search term to add a button
+// Click the button to get gifs and insert into card bodies
+// display rating and title, play/pause feature
 
 $(document).ready(function() {
+    
+    // this is displaying twice so something is wrong
     console.log("hello");
+    
+    // hide the container until there is something to put in it
     $("#gif-container").hide();
-
-    // window.onload = function() {};
 
     // API key from GIPHY
     var APIkey = "BFByOJzeEKQ5fOyhPUyS6VPFT1cjcMx3";
@@ -32,70 +36,64 @@ $(document).ready(function() {
     });
     
     // Take the reponse data and populate the gif cards
-    function addGIFs(x) {
-        for (i = 0; i < 10; i ++) {
-            // variables
+    function addGIFs(response) {
+        for (i = 0; i < 5; i ++) {
+
+            // variables to create divs for data
             var newGifDiv = $("<div class='card' id='each-gif'>");
             var newGIF = $("<img class='card-img-top'>");
             var newGifBody = $("<div class='card-body'>");
             var ratingGIF = $("<p class='card-text'>");
-            var titleGIF = $("<h5 class='card-title>");
-            var sourceURL = x.data[i].images.fixed_height_still.url;
+            var titleGIF = $("<h6 class='card-title'>");
+            var sourceURL = response.data[i].images.fixed_height_still.url;
             
-            //give attributes
-            
+            //give attributes for playing gifs
             newGIF.attr("src", sourceURL);
             newGIF.attr("image-still", sourceURL);
-            newGIF.attr("image-animate", x.data[i].images.fixed_height.url);
+            newGIF.attr("image-animate", response.data[i].images.fixed_height.url);
             newGIF.attr("state", "still");
             
             // append gif card to div and add texts
             $(newGifDiv).append(newGifBody)
             $("#gif-card").prepend(newGifDiv);
-            $(newGifDiv).append(newGIF);
-            //ratingGIF.attr("class", "rating-text");
-            ratingGIF.text("Rating: " + x.data[i].rating);
-            //titleGIF.attr("class", "title-text");
-            titleGIF.text("Title: " + x.data[i].title);
-            $(newGifDiv).append(ratingGIF);
-            $(newGifBody).append(titleGIF);
+            $(newGifDiv).prepend(newGIF);
+            ratingGIF.text("Rating: " + response.data[i].rating);
+            titleGIF.text(response.data[i].title);
+            $(newGifBody).append(ratingGIF);
+            $(newGifBody).prepend(titleGIF);
         } 
     };
 
     // AJAX to the API
     $(document).on("click", ".search-button", function() {
+        
         $("#gif-container").show();
         var titleTag = $(this).attr("data-source");
         var APItag = titleTag;
         var APIURL = "https://api.giphy.com/v1/gifs/search?q=" + APItag + "&api_key=" + APIkey + "&limit=5";
-        console.log(this);
-
+       
         $.ajax({
             url: APIURL,
             method: "GET"
             }).done(function(response) {
                 addGIFs(response);
-                var GIFsTitle = $("<h2>");
-                GIFsTitle.text(titleTag);
-                $("#gif-container").prepend(GIFsTitle);
+                var GIFsTitle = $("<div class='alert alert-success' role='alert'>").text(titleTag);
+                $("#gif-card").prepend(GIFsTitle);
             });
-        console.log(response);
     });
+
 
     // Animate gifs on click
     $(document).on("click", "img", function() {
         
         var GIFstate = $(this).attr("state");
-        console.log(this);
-        console.log(GIFstate);
-
         if (GIFstate === "still") {
-
+            
             $(this).attr("src", $(this).attr("image-animate"));
             $(this).attr("state", "playing");
 
         } else if (GIFstate === "playing") {
-
+            
             $(this).attr("src", $(this).attr("image-animate"));
             $(this).attr("state", "still");
         }
